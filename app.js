4cf1817bb4076e3160ca7cf8cb6c9ca8850acc40
Begum Eryildiz <begumeryildiz@gmail.com>
2022-08-01 11:38:18 +0200

    created task route

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ require("./config")(app);
 // Contrary to the views version, all routes are controlled from the routes/index.js
 app.use("/api", require("./routes/index.routes"));
 app.use("/api", require("./routes/project.routes"));
+app.use('/api', require('./routes/task.routes'));                   
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
