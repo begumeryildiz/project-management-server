cad7c1b88d67aa49c6c836ee5a240b1a3d62086a
Begum Eryildiz <begumeryildiz@gmail.com>
2022-08-01 11:25:45 +0200

    fixed the appjs

--- app.js
+++ app.js
@@ -16,11 +16,8 @@ require("./config")(app);
 
 // 👇 Start handling routes here
 // Contrary to the views version, all routes are controlled from the routes/index.js
-const indexRoutes = require("./routes/index.routes");
-app.use("/api", indexRoutes);
-
-const projectRouter = require('./routes/project.routes');     // <== IMPORT
-app.use('/api', projectRouter);                           // <== ADD
+app.use("/api", require("./routes/index.routes"));
+app.use("/api", require("./routes/project.routes"));
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
